e62ee73c355f7243fb5ad0e33d13394b5684fc35
Gregory Tardivel <gregory.tardivel@gmail.com>
2022-11-16 23:07:06 +0100

    Def for windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+        hostOs == "Mac OS X" -> macosX64("nativeOSX")
+        hostOs == "Linux" -> linuxX64("nativeLinuw")
+        isMingwX64 -> mingwX64("nativeMingw")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
     nativeTarget.apply {
@@ -38,5 +38,15 @@ kotlin {
     sourceSets {
         val commonMain by getting
         val commonTest by getting
+    }
+}
+
+tasks {
+    register("copyDlls", Copy::class) {
+        from("C:\\mingw64\\x86_64-w64-mingw32\\bin") {
+            include("glew32.dll")
+            include("glfw3.dll")
+        }
+        into("build\\bin\\releaseExecutable")
     }
 }
